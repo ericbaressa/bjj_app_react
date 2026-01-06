72e38717b0e1ee8ca3e0ad9fe467eba304d4a9be
ericbaressa <eric.baressa@gmail.com>
2026-01-06 20:16:14 +0100

    Cambio supabase

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,5 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+// URL pública de tu proyecto Supabase
 const supabaseUrl = 'https://kfwqbfkthhftrvtkybcw.supabase.co';
-const supabaseKey = 'sb_publishable_nqJJCCKrYnCpuqCfwDs8SQ_k55qpXV6';
+
+// API key segura (en Vite usamos prefijo VITE_)
+const supabaseKey ='sb_publishable_nqJJCCKrYnCpuqCfwDs8SQ_k55qpXV6';
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
