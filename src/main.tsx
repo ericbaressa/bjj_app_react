dfe34483be5d79e9605bbfb25a604e93aa9ecaa8
ericbaressa <eric.baressa@gmail.com>
2026-01-17 19:11:35 +0100

    prueba1.pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onOfflineReady() {
+    console.log('PWA lista para uso offline')
+  },
+  onNeedRefresh() {
+    console.log('Nueva versión disponible')
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -9,4 +19,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base :'/bjj_app_react/',
-});
+  base: '/bjj_app_react/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mi App Supabase',
+        short_name: 'MiApp',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/bjj_app_react/',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+})
